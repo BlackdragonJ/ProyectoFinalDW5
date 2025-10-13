336f5d4dea290d284c69eb5b0f34979705c46f15
Rodriguez Recinos <62079@bi.com.gt>
2025-10-13 10:52:44 -0600

    ci: add placeholder-data and next.config to the project

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,12 @@
+const invoices = [{
+    customer_id: customers[0].id,
+    amount: 15795,
+    status: 'pending',
+    date: '2025-10-10'
+},
+{
+    customer_id: customers[1].id,
+    amount: 15795,
+    status: 'pending',
+    date: '2025-10-11'
+}];
